4bd204f0975c6e6cc3e6f8b32a62965a1e12eea6
Alan C <alanc@kidney.org>
2016-07-10 16:04:11 -0400

    Added, Styled, and Scripted Bootstrap Modal for Videos

---
+++ src/scripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+ $(".Trailer1").click(function(){
+     $('#myModal').modal('show');
+ });
+
+ $(".Trailer2").click(function(){
+     $('#myModal').modal('show');
+ });
+
+ $(".Trailer3").click(function(){
+     $('#myModal').modal('show');
+ });
+});
