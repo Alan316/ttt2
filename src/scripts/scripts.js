4dc04a27866a9c8931d98f4e22a1f79f375c353a
Alan C <alanc@kidney.org>
2016-07-11 14:59:03 -0400

    Added fix to keep Modal from playing video when closed.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,11 +3,36 @@ $(document).ready(function(){
      $('#Modal1').modal('show');
  });
 
+ $('#Modal1').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+ });
+
+
  $(".Trailer2").click(function(){
      $('#Modal2').modal('show');
  });
 
+ $('#Modal2').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+ });
+
+
  $(".Trailer3").click(function(){
      $('#Modal3').modal('show');
  });
+
+ $('#Modal3').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+ });
+
 });
+
